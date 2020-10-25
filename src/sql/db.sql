ba78303a6b4efd1dc3cbe2ae1cfc659c361795e7
Rahul Mendonca <rahul.mendonca@resolver.com>
2020-10-25 14:30:28 -0400

    SQL files needed for our database

---
+++ src/sql/db.sql
@@ -0,0 +1,30 @@
+/* SQL Commands for database*/
+
+CREATE DATABASE banking;
+
+use banking;
+
+CREATE TABLE  IF NOT EXISTS Users (
+id INT NOT NULL,
+name VARCHAR(10),
+PRIMARY KEY (id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS History (  
+   id INT NOT NULL,  
+   amount INT NOT NULL,	
+   customerId INT NOT NULL,	
+   date DATETIME NOT NULL,
+   PRIMARY KEY (id),
+   CONSTRAINT fk_customerId
+   FOREIGN KEY (customerId) 
+        REFERENCES Users(id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS TestDate (
+   id INT NOT NULL AUTO_INCREMENT,   
+   date DATETIME NOT NULL,
+   PRIMARY KEY (id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8
